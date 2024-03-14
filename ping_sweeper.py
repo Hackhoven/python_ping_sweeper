1a8f7aea4c08fc389ad22feae58365cf9fd483cc
Hackhoven <142750639+Hackhoven@users.noreply.github.com>
2024-03-14 23:28:42 +0400

    Update ping_sweeper.py

--- ping_sweeper.py
+++ ping_sweeper.py
@@ -1,45 +1,37 @@
-# Hackhoven's simple ping sweeper
+# Hackhoven's ping_sweeper
 
 import sys
 import subprocess
 import platform
+import concurrent.futures
 
-def ping_sweeper(ip_range):
-
+def ping_sweeper(ip):
     if platform.system().lower() == 'windows':
         cmd = ['ping', '-n', '1']
     else:
         cmd = ['ping', '-c', '1']
 
-    
-    for i in range(1, 255):
-        ip = f"{ip_range}.{i}"
-        cmd.append(ip)
+    try:
+        result = subprocess.run(cmd + [ip], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-        try:
-            result = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if "Destination host unreachable" in result.stdout:
+            return
+        else:
+            print(f"The host {ip} is up.")
 
-            if "Destination host unreachable" in result.stdout:
-                print(f"The host {ip} is down.")
-            else:
-                print(f"The host {ip} is up.")
-        
-        except subprocess.CalledProcessError:
-            print(f"Error occured with {ip}")
-
-        cmd.pop()
-
+    except subprocess.CalledProcessError:
+        print(f"Error occurred with {ip}")
 
 def main():
-
     if len(sys.argv) != 2:
-        print("Run python3 ping_sweeper.py <IP range>")
+        print("Usage: python3 ping_sweeper.py <IP range>")
         sys.exit(1)
 
     ip_range = sys.argv[1]
 
-    ping_sweeper(ip_range)
-
+    with concurrent.futures.ThreadPoolExecutor(max_workers=64) as executor:
+        ips = [f"{ip_range}.{i}" for i in range(1, 255)]
+        executor.map(ping_sweeper, ips)
 
 if __name__ == "__main__":
     main()
